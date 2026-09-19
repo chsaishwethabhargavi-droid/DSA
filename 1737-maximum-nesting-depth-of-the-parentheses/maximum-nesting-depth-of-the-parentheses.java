948f7b8504eb41b9618a90ae3bc5862dfbe093e7
chsaishwethabhargavi-droid <chsaishwethabhargavi@gmail.com>
2026-09-19 10:39:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (84.84%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count = 0;
+        int max = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                count++;
+                if(count > max) {
+                    max = count;
+                }
+            }
+            if(s.charAt(i) == ')') {
+                count--;
+            }
+        }
+        return max;
+    }
+}
